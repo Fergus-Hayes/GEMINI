584f28ec87a1c7e4e40835f1e5d72ff938759138
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-10-28 01:05:47 -0400

    matlab and octave working for 2D HDF5

--- vis/loadframe_HDF5_3Dcurvavg.m
+++ vis/loadframe_HDF5_3Dcurvavg.m
@@ -31,24 +31,40 @@ assert(exist(fn,'file')==2, [fn,' does not exist '])
 
 simdate=zeros(1,6);    %datevec-style array
 
-simdate(1:3) = h5read(fn, '/time/ymd');
-simdate(4) = h5read(fn, '/time/UThour');
-%% Number densities
-ne = h5read(fn, '/neall');
-%% Parallel Velocities
-v1 = h5read(fn, '/v1avgall');
-%% Temperatures
-Ti = h5read(fn, '/Tavgall');
-Te = h5read(fn, '/TEall');
-%% Current densities
-J1 = h5read(fn, '/J1all');
-J2 = h5read(fn, '/J2all');
-J3 = h5read(fn, '/J3all');
-%% Perpendicular drifts
-v2 = h5read(fn, '/v2avgall');
-v3 = h5read(fn, '/v3avgall');
-%% Topside potential
-Phitop = h5read(fn, '/Phiall');
+if isoctave
+  D = load(fn);
+  simdate(1:3) = D.time.ymd;
+  simdate(4) = D.time.UThour;
+  ne = D.neall;
+  v1 = D.v1avgall;
+  Ti = D.Tavgall;
+  Te = D.TEall;
+  J1 = D.J1all;
+  J2 = D.J2all;
+  J3 = D.J3all;
+  v2 = D.v2avgall;
+  v3 = D.v3avgall;
+  Phitop = D.Phiall;
+else
+  simdate(1:3) = h5read(fn, '/time/ymd');
+  simdate(4) = h5read(fn, '/time/UThour');
+  %% Number densities
+  ne = h5read(fn, '/neall');
+  %% Parallel Velocities
+  v1 = h5read(fn, '/v1avgall');
+  %% Temperatures
+  Ti = h5read(fn, '/Tavgall');
+  Te = h5read(fn, '/TEall');
+  %% Current densities
+  J1 = h5read(fn, '/J1all');
+  J2 = h5read(fn, '/J2all');
+  J3 = h5read(fn, '/J3all');
+  %% Perpendicular drifts
+  v2 = h5read(fn, '/v2avgall');
+  v3 = h5read(fn, '/v3avgall');
+  %% Topside potential
+  Phitop = h5read(fn, '/Phiall');
+end
 %% REORGANIZE ACCORDING TO MATLABS CONCEPT OF A 2D or 3D DATA SET
 if lxs(2) == 1    %a 2D simulations was done in x1 and x3
  % disp('Detected a 2D simulation (x1,x3) and organizing data accordingly.')
