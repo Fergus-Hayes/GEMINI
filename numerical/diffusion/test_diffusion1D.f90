98a317759a5c1bad35cf9cdb45db4b572a494de1
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-09-06 09:13:29 -0400

    diffusion test updated

--- numerical/diffusion/test_diffusion1D.f90
+++ numerical/diffusion/test_diffusion1D.f90
@@ -1,56 +1,53 @@
 program test_diffusion1D
 
-!------------------------------------------------------------
-!-------SOLVE HEAT EQUATION IN 1D
-!------------------------------------------------------------
-use phys_consts, only: wp
+!----------------------------------------------------------------------------
+!-------Solve a time-dependent heat equation in 1D.  See GEMINI-docs repo for
+!-------a description of the specific problem solved here
+!----------------------------------------------------------------------------
+
+use phys_consts, only: wp,pi
 use diffusion
 implicit none
 
-integer, parameter :: npts=25,lt=100
-character(*), parameter :: outfn='test_diffusion1d.dat'
+integer, parameter :: npts=256,lt=1024
+character(*), parameter :: outfile='test_diffusion1d.dat'
 
 real(wp), dimension(npts) :: v1,dx1i
-real(wp), dimension(-1:npts+2) :: x1,Ts
+real(wp), dimension(-1:npts+2) :: x1,Ts,Tstrue
 real(wp), dimension(npts) :: lambda,A,B,C,D,E
 real(wp), dimension(npts+1) :: x1i
 real(wp), dimension(0:npts+2) :: dx1
 integer :: lx1,it,ix1,u
-real(wp) :: t=0,dt
+real(wp) :: t=0.0,dt
 real(wp) :: Tsminx1,Tsmaxx1
 
 
-!include ghost cell locations
-x1=[ (dble(ix1)/dble(npts), ix1=-2,npts+1) ]
-!x1=[ (ix1, ix1=-2,npts+1) ]
+!! create a grid for the calculation
+x1=[ (real(ix1,wp)/real(npts,wp), ix1=-2,npts+1) ]
 lx1=npts   !exclude ghost cells in count
+dx1=x1(0:lx1+2)-x1(-1:lx1+1)
+x1i(1:lx1+1)=0.5*(x1(0:lx1)+x1(1:lx1+1))
+dx1i=x1i(2:lx1+1)-x1i(1:lx1)
 
-  print *,'writing ',outfn
-open(newunit=u,file=outfn,status='replace')
+
+!! write the time, space length adn spatial grid to a file
+print *,'writing ',outfile
+open(newunit=u,file=outfile,status='replace')
 write(u,*) lt
 write(u,*) lx1
 call writearray(u,x1)
 
 
-!IC's
-Ts(:)=0.0
-Ts(11:15)=1.0
-lambda(:)=1.0
-
-!backward diffs
-dx1=x1(0:lx1+2)-x1(-1:lx1+1)
+!! initial conditions
+Ts(-1:lx1+2)=sin(2*pi*x1(-1:lx1+2))
+lambda(:)=1.0_wp     !thermal conductivity
 
 
-!interface data
-x1i(1:lx1+1)=0.5*(x1(0:lx1)+x1(1:lx1+1))
-dx1i=x1i(2:lx1+1)-x1i(1:lx1)
+!! typical diffusion time, make our time step a fraction of this
+dt=0.5_wp*maxval(dx1)**2/maxval(lambda)
 
 
-!typical diffusion time
-!dt=0.1*maxval(dx1)**2/maxval(lambda)
-dt=0.04/lt
-
-
+!! time interations
 do it=1,lt
   !time step
   t=t+dt
@@ -62,18 +59,23 @@ do it=1,lt
   !diffuse
   A(:)=0.0
   B(:)=0.0
-  C(:)=1.0
+  C(:)=1.0_wp
   D(:)=lambda(:)
   E(:)=0.0
   Ts(1:lx1)=backEuler1D(Ts(1:lx1),A,B,C,D,E,Tsminx1,Tsmaxx1,dt,dx1,dx1i)
 
+  !compute analytical solution to compare
+  Tstrue(1:lx1)=exp(-4*pi**2*lambda*t)*sin(2*pi*x1(1:lx1))
+
   !output
   write(u,*) t
   call writearray(u,Ts(1:lx1))
+  call writearray(u,Tstrue(1:lx1))
 end do
 
- close(u)
 
+!! close the file
+close(u)
 
 contains
 
@@ -88,17 +90,17 @@ contains
     end do
   end subroutine writearray
 
-
-  subroutine write2Darray(u,array)
-    integer, intent(in) :: u
-    real(wp), dimension(:,:), intent(in) :: array
-    
-    integer :: k1,k2
-
-    do k1=1,size(array,1)
-      write(u,'(f8.0)') (array(k1,k2), k2=1,size(array,2))
-    end do
-  end subroutine write2Darray
-
+!
+!  subroutine write2Darray(u,array)
+!    integer, intent(in) :: u
+!    real(wp), dimension(:,:), intent(in) :: array
+!    
+!    integer :: k1,k2
+!
+!    do k1=1,size(array,1)
+!      write(u,'(f8.0)') (array(k1,k2), k2=1,size(array,2))
+!    end do
+!  end subroutine write2Darray
+!
 
 end program test_diffusion1D
--- numerical/diffusion/test_diffusion1D.m
+++ numerical/diffusion/test_diffusion1D.m
@@ -22,19 +22,29 @@ t=zeros(lt,1);
 for it=1:lt
   t(it)=fscanf(fid,'%f',1);
   Ts(:,it)=fscanf(fid,'%f',lx1)';
+  Tstrue(:,it)=fscanf(fid,'%f',lx1)';
 end % for
 
 % reltol = 1e-5 for real32
-assert_allclose(Ts(13,end), 0.2757552094055,1e-5,[],'1-D diffusion accuracy')
+%assert_allclose(Ts(13,end), 0.2757552094055,1e-5,[],'1-D diffusion accuracy')
 
 if ~isinteractive, return, end
 %% plots
+
 figure
+subplot(121);
 imagesc(t,x1(3:end-2),Ts)
 colorbar
-xlabel('time [sec]')
-ylabel('displacement [m]')
-title('1-D diffusion (vs. time)')
+xlabel('time (sec)')
+ylabel('distance (m)')
+title('1D diffusion (numerical)')
+
+subplot(122);
+imagesc(t,x1(3:end-2),Tstrue)
+colorbar
+xlabel('time (sec)')
+ylabel('distance (m)')
+title('1D diffusion (analytical)')
 
 
 end % function
