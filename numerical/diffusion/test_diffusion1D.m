0233be24874945bba9300598d49fbdeb5d8a93c8
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-09-16 14:49:27 -0400

    python/matlab: update diffusion test scripts

--- numerical/diffusion/test_diffusion1D.m
+++ numerical/diffusion/test_diffusion1D.m
@@ -3,9 +3,11 @@
 %look over the differences and numerical error.
 function [x1,TsEuler,TsBDF2,Tstrue]=test_diffusion1D(fn)
 
+narginchk(1,1)
 cwd = fileparts(mfilename('fullpath'));
-addpath([cwd,filesep,'../../tests'])
-addpath([cwd,filesep,'../../script_utils'])
+for p = {'tests', 'script_utils', 'vis'}
+  addpath([cwd,filesep,'..',filesep,'..', filesep, p{:}])
+end
 
 if exist(fn, 'file') ~= 2, fprintf(2, [fn, ' not found\n']), exit(77), end
 
@@ -19,7 +21,6 @@ x1=fscanf(fid,'%f',lx1+4)';
 %M=reshape(M,[lx1 5])';
 %b=fscanf(fid,'%f',lx1)';
 
-Ts=zeros(lx1,lt);
 t=zeros(lt,1);
 for it=1:lt
   t(it)=fscanf(fid,'%f',1);
@@ -29,33 +30,37 @@ for it=1:lt
 end % for
 
 % reltol = 1e-5 for real32
-%assert_allclose(Ts(13,end), 0.2757552094055,1e-5,[],'1-D diffusion accuracy')
+% this is just a random point we're comparing.
+assert_allclose(TsEuler(13, 13), 0.770938954253086, 1e-5,[],'1-D Euler diffusion accuracy')
+assert_allclose(TsBDF2(13, 13),  0.763236513549944, 1e-5,[],'1-D BDF2 diffusion accuracy')
+assert_allclose(Tstrue(13, 13),  0.763014494788105, 1e-5,[],'1-D true diffusion accuracy')
 
-if ~isinteractive, return, end
+if ~isinteractive
+  if ~nargout, clear, end
+  return
+end
 %% plots
 
 figure
-subplot(131);
-imagesc(t,x1(3:end-2),TsEuler)
-colorbar
-xlabel('time (sec)')
-ylabel('distance (m)')
-title('1D diffusion (backward Euler)')
+ax1=subplot(1,3,1);
+imagesc(t,x1(3:end-2),TsEuler, 'parent', ax1)
+ylabel(ax1,'distance (m)')
+title(ax1,'1D diffusion (backward Euler)')
 
-subplot(132);
-imagesc(t,x1(3:end-2),TsBDF2)
-colorbar
-xlabel('time (sec)')
-ylabel('distance (m)')
-title('1D diffusion (TRBDF2)')
+ax2=subplot(1,3,2);
+imagesc(t,x1(3:end-2),TsBDF2, 'parent', ax2)
+title(ax2,'1D diffusion (TRBDF2)')
 
-subplot(133);
-imagesc(t,x1(3:end-2),Tstrue)
-colorbar
-xlabel('time (sec)')
-ylabel('distance (m)')
-title('1D diffusion (analytical)')
+ax3=subplot(1,3,3);
+imagesc(t,x1(3:end-2),Tstrue, 'parent', ax3)
+title(ax3,'1D diffusion (analytic)')
 
+for ax = [ax1,ax2,ax3]
+  colormap(ax, 'bwr')
+  colorbar('peer',ax)
+  xlabel(ax,'time (sec)')
+end
 
+if ~nargout, clear, end
 end % function
 
